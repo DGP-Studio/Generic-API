0fc81364aa11aa1c35b56b638ea89cd87222e49d
Masterain <i@irain.in>
2024-03-07 15:40:28 -0800

    adapt new static package rules

--- routers/static.py
+++ routers/static.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, Response
+import logging
+
+from fastapi import APIRouter, Depends, Response, Request, HTTPException
 from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from mysql_app import schemas
@@ -19,16 +21,40 @@ cn_raw_url = "https://jihulab.com/DGP-Studio/Snap.Static/-/raw/main/{file_path}"
 
 
 @china_router.get("/zip/{file_path:path}")
-async def cn_get_zipped_file(file_path: str):
+async def cn_get_zipped_file(file_path: str, request: Request):
     """
     Endpoint used to redirect to the zipped static file in China server
 
+    :param request: request object from FastAPI
     :param file_path: File relative path in Snap.Static.Zip
 
     :return: 302 Redirect to the zip file
     """
     # https://jihulab.com/DGP-Studio/Snap.Static.Zip/-/raw/main/{file_path}
     # https://static-next.snapgenshin.com/d/zip/{file_path}
+    quality = request.headers.get("x-quality", "unknown").lower()
+    minimum_package = request.headers.get("x-minimum", "unknown").lower()
+
+    if quality == "unknown" or minimum_package == "unknown":
+        raise HTTPException(status_code=418, detail="Invalid request")
+
+    match minimum_package:
+        case "true":
+            if file_path == "ItemIcon.zip" or file_path == "EmotionIcon.zip":
+                file_path = file_path.replace(".zip", "-Minimum.zip")
+        case "false":
+            pass
+        case _:
+            raise HTTPException(status_code=404, detail="Invalid minimum package")
+
+    match quality:
+        case "high":
+            file_path = file_path.replace(".zip", "-tiny.zip")
+        case "raw":
+            pass
+        case _:
+            raise HTTPException(status_code=404, detail="Invalid quality")
+    logging.debug(f"Redirecting to {cn_zip_url.format(file_path=file_path)}")
     return RedirectResponse(cn_zip_url.format(file_path=file_path), status_code=302)
 
 
@@ -47,14 +73,37 @@ async def cn_get_raw_file(file_path: str):
 
 
 @global_router.get("/zip/{file_path:path}")
-async def global_get_zipped_file(file_path: str):
+async def global_get_zipped_file(file_path: str, request: Request):
     """
     Endpoint used to redirect to the zipped static file in Global server
 
+    :param request: request object from FastAPI
     :param file_path: Relative path in Snap.Static.Zip
 
     :return: Redirect to the zip file
     """
+    quality = request.headers.get("x-quality", "unknown").lower()
+    minimum_package = request.headers.get("x-minimum", "unknown").lower()
+
+    if quality == "unknown" or minimum_package == "unknown":
+        raise HTTPException(status_code=418, detail="Invalid request")
+
+    match minimum_package:
+        case "true":
+            file_path = file_path.replace(".zip", "-Minimum.zip")
+        case "false":
+            pass
+        case _:
+            raise HTTPException(status_code=404, detail="Invalid minimum package")
+
+    match quality:
+        case "high":
+            file_path = file_path.replace(".zip", "-tiny.zip")
+        case "raw":
+            pass
+        case _:
+            raise HTTPException(status_code=404, detail="Invalid quality")
+    logging.debug(f"Redirecting to https://static-zip.snapgenshin.cn/{file_path}")
     return RedirectResponse(f"https://static-zip.snapgenshin.cn/{file_path}", status_code=302)
 
 
