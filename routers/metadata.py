54433b3ab3f30ac1190b80e2ce36feace89c9fd1
Masterain <i@irain.in>
2024-04-12 22:38:13 -0700

    Update metadata module

--- routers/metadata.py
+++ routers/metadata.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 import redis
 import json
@@ -6,11 +5,11 @@ from fastapi import APIRouter, Depends
 from fastapi.responses import RedirectResponse
 from fastapi_utils.tasks import repeat_every
 from utils.dgp_utils import validate_client_is_updated
+from utils.redis_utils import redis_conn
 from base_logger import logger
-
+from mysql_app.schemas import StandardResponse
 
 scan_duration = int(os.getenv("CENSOR_FILE_SCAN_DURATION", 30)) / 2  # half of the duration
-metadata_censored_files = []
 
 china_router = APIRouter(tags=["Hutao Metadata"], dependencies=[Depends(validate_client_is_updated)],
                          prefix="/metadata")
@@ -18,47 +17,27 @@ global_router = APIRouter(tags=["Hutao Metadata"], dependencies=[Depends(validat
                           prefix="/metadata")
 
 
-@repeat_every(seconds=60 * scan_duration)
-async def refresh_metadata_censored_files():
-    """
-    Refresh metadata_censored_files every 30 minutes.
-    """
-    if os.getenv("NO_REDIS", "false").lower() == "true":
-        logger.info("Skipping scheduled refreshing metadata_censored_files as NO_REDIS is set to true")
-        return True
-    logger.info(f"Start {scan_duration * 60}-min scheduled refreshing metadata_censored_files")
-    global metadata_censored_files
-    if os.getenv("NO_REDIS", "false").lower() == "true":
-        logger.info("Skipping Redis connection in Wallpaper module as NO_REDIS is set to true")
-        r = None
-    else:
-        REDIS_HOST = os.getenv("REDIS_HOST", "redis")
-        logger.info(f"Connecting to Redis at {REDIS_HOST}")
-        r = redis.Redis(host=REDIS_HOST, port=6379, db=1, decode_responses=True)
-        logger.info("Redis connection established")
-    redis_data = await r.get("metadata_censored_files")
-    logger.info(f"Receive data of metadata_censored_files from Redis: {redis_data}")
-    if redis_data is not None:
-        metadata_censored_files = json.loads(redis_data)
-    else:
-        metadata_censored_files = []
-    r.close()
-    logger.info(f"Redis connection closed as scheduled refreshing metadata_censored_files finished")
-
-
-asyncio.ensure_future(refresh_metadata_censored_files())
-
-
-@china_router.get("/ban")
-@global_router.get("/ban")
-async def get_banned_files():
+def get_banned_files() -> dict | None:
     """
     Get the list of censored files.
 
     :return: a list of censored files
     """
-    global metadata_censored_files
-    return metadata_censored_files
+    metadata_censored_files = redis_conn.get("metadata_censored_files")
+    if metadata_censored_files:
+        return json.loads(metadata_censored_files)
+    return None
+
+
+@china_router.get("/ban", response_model=StandardResponse)
+@global_router.get("/ban", response_model=StandardResponse)
+async def get_ban_files_endpoint() -> StandardResponse:
+    """
+    Get the list of censored files. [FastAPI Endpoint]
+
+    :return: a list of censored files in StandardResponse format
+    """
+    return StandardResponse(data=get_banned_files())
 
 
 @china_router.get("/{file_path:path}")
@@ -73,7 +52,7 @@ async def china_metadata_request_handler(file_path: str):
     host_for_normal_files = f"https://jihulab.com/DGP-Studio/Snap.Metadata/-/raw/main/{file_path}"
     host_for_censored_files = f"https://metadata.snapgenshin.com/{file_path}"
 
-    if file_path in metadata_censored_files:
+    if file_path in get_banned_files():
         return RedirectResponse(host_for_censored_files, status_code=302)
     else:
         return RedirectResponse(host_for_normal_files, status_code=302)
