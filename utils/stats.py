3b1d83f2d8c30c61d41fe7b1c57e76d839557fb4
Masterain <i@irain.in>
2024-06-09 23:27:02 -0700

    deprecate x-device-id

--- utils/stats.py
+++ utils/stats.py
@@ -16,16 +16,15 @@ else:
     logger.info("Redis connection established for Stats module (db=2)")
 
 
-def record_device_id(x_device_id: Optional[str] = Header(None), x_region: Optional[str] = Header(None),
-                     x_hutao_device_id: Optional[str] = Header(None), user_agent: Optional[str] = Header(None)) -> bool:
-    start_time = time.time()  # 记录开始时间
+def record_device_id(x_region: Optional[str] = Header(None), x_hutao_device_id: Optional[str] = Header(None),
+                     user_agent: Optional[str] = Header(None)) -> bool:
+    start_time = time.time()
 
     if not redis_conn:
         logger.warning("Redis connection not established, not recording device ID")
         return False
 
-    captured_device_id = x_hutao_device_id or x_device_id
-    if not captured_device_id:
+    if not x_hutao_device_id:
         logger.info(f"Device ID not found in headers, not recording device ID")
         return False
 
@@ -34,18 +33,18 @@ def record_device_id(x_device_id: Optional[str] = Header(None), x_region: Option
         "global": "active_users_global"
     }.get((x_region or "").lower(), "active_users_unknown")
 
-    redis_conn.sadd(redis_key_name, captured_device_id)
+    redis_conn.sadd(redis_key_name, x_hutao_device_id)
 
     if user_agent:
         user_agent = user_agent.replace("Snap Hutao/", "")
-        patch_redis_conn.sadd(user_agent, captured_device_id)
+        patch_redis_conn.sadd(user_agent, x_hutao_device_id)
 
-        end_time = time.time()  # 记录结束时间
+        end_time = time.time()
         execution_time = (end_time - start_time) * 1000
         print(f"Execution time[1]: {execution_time} ms")
         return True
 
-    end_time = time.time()  # 记录结束时间
+    end_time = time.time()
     execution_time = (end_time - start_time) * 1000
     print(f"Execution time[2]: {execution_time} ms")
 
