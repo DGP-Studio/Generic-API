fc84a6c577f383079c934090ea3fc246dac579ba
Masterain <i@irain.in>
2024-10-05 14:34:40 -0700

    Update database.py

--- mysql_app/database.py
+++ mysql_app/database.py
@@ -5,13 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from base_logger import logging
 import socket
 
-try:
-    host_ip = socket.gethostbyname('host.docker.internal')
-    print("Host IP:", host_ip)
-except socket.gaierror:
-    print("Could not resolve host.docker.internal")
 
-MYSQL_HOST = 'host.docker.internal'
+MYSQL_HOST = socket.gethostbyname('host.docker.internal')
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
