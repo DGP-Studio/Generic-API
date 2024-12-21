a1cd2a81b48f59ff336ca95b376d15a89c43131a
Masterain <i@irain.in>
2024-12-20 16:31:08 -0800

    add Homa authentication

--- utils/authentication.py
+++ utils/authentication.py
@@ -1,7 +1,9 @@
 from fastapi import HTTPException, Header
 from typing import Annotated
-from config import API_TOKEN
+from config import API_TOKEN, HOMA_SERVER_IP
 from base_logger import logger
+from mysql_app.homa_schemas import HomaPassport
+import httpx
 
 
 def verify_api_token(api_token: Annotated[str, Header()]) -> bool:
@@ -11,3 +13,25 @@ def verify_api_token(api_token: Annotated[str, Header()]) -> bool:
     else:
         logger.error(f"API token is invalid: {api_token}")
         raise HTTPException(status_code=403, detail="API token is invalid.")
+
+
+def verify_homa_user_level(homa_token: Annotated[str, Header()]) -> HomaPassport:
+    if HOMA_SERVER_IP is None:
+        logger.error("Homa server IP is not set.")
+        raise HTTPException(status_code=500, detail="Homa server IP is not set.")
+    if homa_token is None:
+        return HomaPassport(user_name="Anonymous", is_developer=False, is_maintainer=False, sponsor_expire_date=None)
+    url = f"http://{HOMA_SERVER_IP}/Passport/UserInfo"
+    headers = {
+        "Authorization": f"Bearer {homa_token}",
+        "User-Agent": "Hutao Generic API"
+    }
+    response = httpx.get(url, headers=headers)
+    if response.status_code == 200:
+        response = response.json()
+        return HomaPassport(
+            user_name=response["UserName"],
+            is_developer=response["IsLicensedDeveloper"],
+            is_maintainer=response["IsMaintainer"],
+            sponsor_expire_date=response["GachaLogExpireAt"]
+        )
