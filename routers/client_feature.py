ebb4d2e1afe911b8a2ab89783713740db990332a
Masterain <i@irain.in>
2025-06-29 20:27:01 -0700

    Update

--- routers/client_feature.py
+++ routers/client_feature.py
@@ -9,17 +9,15 @@ global_router = APIRouter(tags=["Client Feature"], prefix="/client")
 fujian_router = APIRouter(tags=["Client Feature"], prefix="/client")
 
 
-@china_router.get("/{file_path:path}", dependencies=[Depends(enhanced_safety_check)])
-async def china_client_feature_request_handler(request: Request, file_path: str) -> RedirectResponse:
-    """
-    Handle requests to client feature metadata files.
+@china_router.get("/{file_path:path}")
+async def china_client_feature_request_handler(
+    request: Request,
+    file_path: str,
+    safety_check: bool | RedirectResponse = Depends(enhanced_safety_check)
+) -> RedirectResponse:
+    if isinstance(safety_check, RedirectResponse):
+        return safety_check
 
-    :param request: Request object from FastAPI
-
-    :param file_path: Path to the metadata file
-
-    :return: HTTP 301 redirect to the file based on censorship status of the file
-    """
     redis_client = aioredis.Redis.from_pool(request.app.state.redis)
 
     host_for_normal_files = await redis_client.get("url:china:client-feature")
@@ -28,17 +26,15 @@ async def china_client_feature_request_handler(request: Request, file_path: str)
     return RedirectResponse(host_for_normal_files, status_code=301)
 
 
-@global_router.get("/{file_path:path}", dependencies=[Depends(enhanced_safety_check)])
-async def global_client_feature_request_handler(request: Request, file_path: str) -> RedirectResponse:
-    """
-    Handle requests to client feature metadata files.
+@global_router.get("/{file_path:path}")
+async def global_client_feature_request_handler(
+    request: Request,
+    file_path: str,
+    safety_check: bool | RedirectResponse = Depends(enhanced_safety_check)
+) -> RedirectResponse:
+    if isinstance(safety_check, RedirectResponse):
+        return safety_check
 
-    :param request: Request object from FastAPI
-
-    :param file_path: Path to the metadata file
-
-    :return: HTTP 301 redirect to the file based on censorship status of the file
-    """
     redis_client = aioredis.Redis.from_pool(request.app.state.redis)
 
     host_for_normal_files = await redis_client.get("url:global:client-feature")
@@ -47,17 +43,15 @@ async def global_client_feature_request_handler(request: Request, file_path: str
     return RedirectResponse(host_for_normal_files, status_code=301)
 
 
-@fujian_router.get("/{file_path:path}", dependencies=[Depends(enhanced_safety_check)])
-async def fujian_client_feature_request_handler(request: Request, file_path: str) -> RedirectResponse:
-    """
-    Handle requests to client feature metadata files.
+@fujian_router.get("/{file_path:path}")
+async def fujian_client_feature_request_handler(
+    request: Request,
+    file_path: str,
+    safety_check: bool | RedirectResponse = Depends(enhanced_safety_check)
+) -> RedirectResponse:
+    if isinstance(safety_check, RedirectResponse):
+        return safety_check
 
-    :param request: Request object from FastAPI
-
-    :param file_path: Path to the metadata file
-
-    :return: HTTP 301 redirect to the file based on censorship status of the file
-    """
     redis_client = aioredis.Redis.from_pool(request.app.state.redis)
 
     host_for_normal_files = await redis_client.get("url:fujian:client-feature")
